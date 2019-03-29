a812aeabe6eb1107464e44dbde7b958b079c0b5a
Christopher Fonseka <chris.fonseka@googlemail.com>
2019-03-29 17:53:28 +0000

    Initial Commit

---
+++ Multiplicative Persistence/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Multiplicative Persistence
+//
+//  Created by Christopher Fonseka on 29/03/2019.
+//  Copyright © 2019 Christopher Fonseka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
